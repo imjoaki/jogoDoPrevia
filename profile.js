b214412ca602414011e97615c429f37a2c33daf6
alexis2341 <alexisvargas61@gmail.com>
2021-10-28 09:59:00 -0300

    mardarCosas

---
+++ profile.js
@@ -0,0 +1,11 @@
+function validar(){
+let nombre = document.getElementById("nombre");
+let departamento = document.getElementById("departamento");
+let acepto1 = document.getElementById("acepto1");
+let acepto2 = document.getElementById("acepto2");
+let elNumero = document.getElementById("elNumero");
+let mandarCosas = document.getElementById("mandarCosas");
+
+
+
+}
